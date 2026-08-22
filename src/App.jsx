adf62ec49d0308b03a53ba04ba2a353a49c82171
starinterio21 <starinterio21@gmail.com>
2026-08-22 07:46:18 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Dashboard from './pages/Dashboard'
 import Inventory from './pages/Inventory'
 import LowStock from './pages/LowStock'
 import OutOfStock from './pages/OutOfStock'
-import NotCounted from './pages/NotCounted'
 import StockIn from './pages/StockIn'
 import StockOut from './pages/StockOut'
 import StockAdjustment from './pages/StockAdjustment'
@@ -25,7 +24,6 @@ export default function App() {
         <Route path="/inventory" element={<ProtectedRoute><Inventory /></ProtectedRoute>} />
         <Route path="/low-stock" element={<ProtectedRoute><LowStock /></ProtectedRoute>} />
         <Route path="/out-of-stock" element={<ProtectedRoute><OutOfStock /></ProtectedRoute>} />
-        <Route path="/not-counted" element={<ProtectedRoute><NotCounted /></ProtectedRoute>} />
         <Route path="/stock-in" element={<ProtectedRoute><StockIn /></ProtectedRoute>} />
         <Route path="/stock-out" element={<ProtectedRoute><StockOut /></ProtectedRoute>} />
         <Route
